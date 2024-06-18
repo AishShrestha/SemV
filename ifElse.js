2005d2d346cb8b08a618e8344f214aa510ff0942
AishShrestha <shresthaayush49@gmail.com>
2024-06-18 08:07:05 +0545

    controller practice

---
+++ ifElse.js
@@ -0,0 +1,16 @@
+const num = 49;
+
+if(num >50)
+    {
+        console.log(`The given number is more than 50`);
+    }
+    else{
+        console.log(`The number is less than or equal to 50`);
+    }
+
+ if(num%2 == 0){
+    console.log("The given number is even");
+ }
+ else {
+    console.log("The given number is odd");
+ }
---
+++ passFail.js
@@ -0,0 +1,16 @@
+const per = prompt("Enter your percentage:");
+
+if (per <= 50) {
+    document.write("Fail")
+    // console.log("Fail");
+}
+else if (per > 50 && per <= 60) {
+    document.write("Second division");
+    // console.log("Second division");
+}
+else {
+    document.write("Distintion");
+    // console.log("Distintion");
+}
+
+
